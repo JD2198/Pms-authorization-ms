ef5f5f860010a8c041e6f6b74808e687fd5b8b6d
unknown <jha21dibya@gmail.com>
2022-09-03 18:33:51 +0530

    initial commit

---
+++ src/main/java/com/authorizationService/controller/authorizationController.java
@@ -0,0 +1,68 @@
+package com.authorizationService.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.authorizationService.service.MyUserDetailsService;
+import com.authorizationService.util.AuthenticationRequest;
+import com.authorizationService.util.AuthenticationResponse;
+import com.authorizationService.util.JwtUtil;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class authorizationController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(authorizationController.class);
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtTokentUtil;
+
+	@GetMapping("/validate")
+	public String isValid() {
+
+		LOGGER.info("Received Request to validate");
+
+		return "valid";
+
+	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			LOGGER.info("Received Request for user authentication");
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+
+			LOGGER.info("Autentication failed (Incorrect username or password)");
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokentUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+	}
+
+}
---
+++ src/main/java/com/authorizationService/util/JwtRequestFilter.java
@@ -0,0 +1,97 @@
+package com.authorizationService.util;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.authorizationService.service.MyUserDetailsService;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JwtRequestFilter.class);
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
+
+		String path = request.getRequestURI();
+		System.out.println(path);
+		if ("/api/authorization-service/manage/health".equals(path)) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		final String authorizationHeader = request.getHeader("Authorization");
+
+		String username = null;
+		String jwt = null;
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			jwt = authorizationHeader.substring(7);
+			try {
+				username = jwtUtil.extractUsername(jwt);
+			} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException
+					| IllegalArgumentException e) {
+
+				LOGGER.info("Invalid jwt token !!!");
+
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token expired");
+
+			}
+
+		}
+
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+			try {
+				if (jwtUtil.validateToken(jwt, userDetails)) {
+
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							userDetails, null, userDetails.getAuthorities());
+					usernamePasswordAuthenticationToken
+							.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+					LOGGER.info("Authentication Successful !!!");
+
+				}
+			} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException
+					| IllegalArgumentException e) {
+
+				LOGGER.info("Invalid jwt token !!!");
+
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token expired");
+
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+}
